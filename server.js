4cb3374dcca6255a64c55e1adb36d78edcb69e32
maxmccord <maxmccord93@gmail.com>
2017-01-14 15:38:47 -0600

    Creating initial 'under construction' site.

---
+++ server.js
@@ -0,0 +1,25 @@
+// Author: Max McCord
+// Date:   Jan 14, 2017
+
+var fs = require('fs');
+var express = require('express');
+
+var PORT = process.argv[2] || 80;
+
+var app = express();
+
+app.get('/', function (req, res) {
+   fs.readFile('html/index.html', function (err, data) {
+      if (err) return res.status(500).send('Could not load file.');
+      res.send(data.toString());
+   });
+});
+
+app.listen(PORT, function (err) {
+   if (err) {
+      console.log('Could not start server!');
+      return;
+   }
+
+   console.log('Listening on port ' + PORT + '...');
+});
